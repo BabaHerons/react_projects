127f80d9c65ea66de5a6dfad4178eafa24a76f4c
Manmay <manmay.singh@gmail.com>
2025-09-03 01:03:04 +0530

    feat: integrate Tailwind CSS and refactor Todo app components
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Refactored App.css to utilize Tailwind CSS for styling.
    - Updated App.tsx to implement a Todo form with Tailwind CSS classes.
    - Created a new interface for Todo items and added state management for todos.
    - Simplified the form submission process and input handling.
    - Removed unused styles and components from the original setup.

--- todo_simple/src/App.tsx
+++ todo_simple/src/App.tsx
@@ -1,33 +1,91 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+interface toDo {
+  task_name: string,
+  description: string,
+  status: boolean,
+  date: Date | string
+}
+
+const makeEmptyTodo = ():toDo => ({
+  task_name: "",
+  description: "",
+  status: false,
+  date: ""
+})
+
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  // const [todo, setTodo] = useState<toDo>({
+  //   task_name: "",
+  //   description: "",
+  //   status: false,
+  //   date: ""
+  // })
+
+  const [todo, setTodo] = useState<toDo>(makeEmptyTodo())
+
+  const [todos, setTodos] = useState<toDo[]>([])
+
+  function submit() {
+    const newTodo = {
+      ...todo,
+      date: new Date().toISOString()
+    }
+
+    const updatedTodos = [
+      ...todos,
+      newTodo
+    ]
+    setTodos(updatedTodos)
+    console.log("Todo: ", newTodo)
+    console.log("Todo List: ", updatedTodos)
+
+    // Resetting the form
+    // setTodo({
+    //   task_name: "",
+    //   description: "",
+    //   status: false,
+    //   date: ""
+    // })
+    setTodo(makeEmptyTodo())
+  }
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>){
+    const { value, name } = e.target ?? {}
+    // console.log(name, value)
+    // console.log(e.target)
+    setTodo((prev:toDo) => ({
+      ...prev,
+      [name]: value
+    }))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container m-auto '>
+        <h1 className='text-5xl font-mono text-rose-400 font-bold my-4 flex justify-center'>Todo Tracker</h1>
+        <span className='font-bold text-2xl bg-purple-200 p-2 rounded-2xl font-sans'>Add a todo</span>
+        <div className=' flex justify-center rounded-2xl mt-4'>
+          <form className='flex border rounded-lg p-4'>
+            {/* Task name */}
+            <div className='m-4'>
+              <p className='font-bold'>Task Name</p>
+              <input className='input' type="text" name='task_name' value={todo.task_name} placeholder='Add name' onChange={handleChange}/>
+            </div>
+
+            {/* Task Description */}
+            <div className='m-4'>
+              <p className='font-bold'>Task Description</p>
+              <input className='input' type="text" name='description' value={todo.description} placeholder='Add description' onChange={handleChange}/>
+            </div>
+
+            {/* Button */}
+            <button type='button' className='btn bg-amber-200 hover:bg-amber-300' onClick={submit}>Submit</button>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React + Typescript</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
