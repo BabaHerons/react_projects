0553d03c9554ff8d8064877128b9742b0afa61a7
Manmay <manmay.singh@gmail.com>
2025-09-03 02:01:46 +0530

    feat: enhance Todo app UI with form labels and improved table layout

--- todo_simple/src/App.tsx
+++ todo_simple/src/App.tsx
@@ -67,27 +67,89 @@ function App() {
       <div className='container m-auto '>
         <h1 className='text-5xl font-mono text-rose-400 font-bold my-4 flex justify-center'>Todo Tracker</h1>
         <span className='font-bold text-2xl bg-purple-200 p-2 rounded-2xl font-sans'>Add a todo</span>
+
+        {/* FORM FOR TODO */}
         <div className=' flex justify-center rounded-2xl mt-4'>
           <form className='flex border rounded-lg p-4'>
             {/* Task name */}
-            <div className='m-4'>
-              <p className='font-bold'>Task Name</p>
-              <input className='input' type="text" name='task_name' value={todo.task_name} placeholder='Add name' onChange={handleChange}/>
+            <div className="m-4">
+              <p className="font-bold">Task Name</p>
+              <input
+                className="input"
+                type="text"
+                name="task_name"
+                value={todo.task_name}
+                placeholder="Add name"
+                onChange={handleChange}
+              />
             </div>
 
             {/* Task Description */}
-            <div className='m-4'>
-              <p className='font-bold'>Task Description</p>
-              <input className='input' type="text" name='description' value={todo.description} placeholder='Add description' onChange={handleChange}/>
+            <div className="m-4">
+              <p className="font-bold">Task Description</p>
+              <input
+                className="input"
+                type="text"
+                name="description"
+                value={todo.description}
+                placeholder="Add description"
+                onChange={handleChange}
+              />
             </div>
 
             {/* Button */}
-            <button type='button' className='btn bg-amber-200 hover:bg-amber-300' onClick={submit}>Submit</button>
+            <button
+              type="button"
+              className="btn bg-amber-200 hover:bg-amber-300"
+              onClick={submit}
+            >
+              Submit
+            </button>
           </form>
+        </div>
+
+        {/* TABLE SHOING THE TODOS */}
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-4">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+            <caption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white">
+              TODOS
+              <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of all the todos at one place.</p>
+            </caption>
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+              <tr>
+                <th scope="col" className="px-6 py-3">S.No.</th>
+                <th scope="col" className="px-6 py-3">Name</th>
+                <th scope="col" className="px-6 py-3">Description</th>
+                <th scope="col" className="px-6 py-3">Status</th>
+                <th scope="col" className="px-6 py-3">Date Added</th>
+                <th scope="col" className="px-6 py-3"><span className="sr-only">Edit</span></th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                todos.length === 0 ? (
+                  <tr>
+                    <td colSpan={6} className="px-6 py-4 text-center text-gray-400">No todos yet</td>
+                  </tr>
+                ) : (
+                  todos.map((t, index) => (
+                    <tr key={String(index) + '_todo'} className="bg-white border-b  border-gray-200">
+                      <td className="px-6 py-4">{index + 1}.</td>
+                      <td className="px-6 py-4">{t.task_name}</td>
+                      <td className="px-6 py-4">{t.description}</td>
+                      <td className="px-6 py-4">{String(t.status)}</td>
+                      <td className="px-6 py-4">{t.date.toLocaleString()}</td>
+                      <td className="px-6 py-4 text-right"><a href="#" className="font-medium text-blue-600 hover:underline">Edit</a></td>
+                    </tr>
+                  ))
+                )
+              }
+            </tbody>
+          </table>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
